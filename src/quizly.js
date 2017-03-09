41f716b973549e49a7713d0a7a30365eceb6c367
samuraiseoul <scottlavigne28@gmail.com>
2017-03-08 21:19:53 -0600

    converte javascript and jquery to typescript. complete #17

--- src/quizly.js
+++ src/quizly.js
@@ -1,64 +1,78 @@
-function Quizly(id, data){
-    this.container = $('#' + id);
-    var self = this;
-    var template = new QuizlyTemplate(this.container);
-
-    var getValues = function($input, self){
-      var typeAttr = $input.attr('type');
-      var value = null;
-
-      if(typeAttr && typeAttr.toLowerCase() == 'checkbox'){
-        var checked = self.container.find('input[name='+$input.attr('name')+']:checked');
-        var values = [];
-        checked.each(function(){
-          values.push($(this).val());
-        });
-        value = values;
-      } else {
-        value = $input.val();
-      }
-      return value instanceof Array ? value : [value];
-    };
-
-    var handler = function($input){
-      var values = getValues($input, self);
-      var $container = $input.parents('div[data-quiz-container]').length? $input.parents('div[data-quiz-container]') : $input.parent();
-      var answer = $container.find('[data-answer]').attr('data-answer');
-      var answers = answer.includes(',') ? answer.split(',') : [answer];
-
-      var correct = true;
-      for(var i =0; i < values.length; i++){
-        if(answers.indexOf(values[i]) == -1){
-          correct = false;
-          break;
+///<reference path="./template.ts"/>
+var Quizly = (function () {
+    function Quizly(id, data) {
+        this.container = document.getElementById(id);
+        this.self = this;
+        var that = this;
+        this.template = new QuizlyTemplater(this.container);
+        if (data) {
+            this.createQuizHtml(this.container, data);
         }
-      }
-      $container.find('.right, .wrong').hide();
-      if((correct && (values.length == answers.length))) {
-        $container.find('.right').show();
-      } else {
-          $container.find('.wrong').show();
-      }
+        [].forEach.call(this.container.querySelectorAll('input, select'), function (el) {
+            el.quizly = that;
+            el.addEventListener('change', that.handler);
+        });
+        var resultSpans = this.container.querySelectorAll('.right, .wrong');
+        for (var i = 0; i < resultSpans.length; i++) {
+            resultSpans[i].style.display = 'none';
+        }
+    }
+    Quizly.prototype.getValues = function (input, self) {
+        var typeAttr = input.getAttribute('type');
+        var value = null;
+        if (typeAttr && typeAttr.toLowerCase() == 'checkbox') {
+            var checked = input.quizly.container.querySelectorAll('input[name=' + input.getAttribute('name') + ']:checked');
+            var values = [];
+            for (var i = 0; i < checked.length; i++) {
+                values.push(checked[i].value);
+            }
+            value = values;
+        }
+        else {
+            value = input.value;
+        }
+        return value instanceof Array ? value : [value];
     };
-
-    var createQuizHtml = function(container, data){
-      for(var i = 0; i < data.length; i++){
-        var question = data[i];
-        if(question.type == "select") {
-          template.createSelect(question);
-        } else {
-            if(question.type == "radio" || question.type == "checkbox") {
-              template.createCheckboxOrRadio(question);
-            } else {
-              template.createInput(question);
+    Quizly.prototype.handler = function (event) {
+        var input = event.srcElement;
+        var values = input.quizly.getValues(input, self);
+        var container = input.parentNode.parentNode !== null ? input.parentNode.parentNode : input.parentNode;
+        var answer = container.querySelector('[data-answer]').getAttribute('data-answer');
+        var answers = answer.includes(',') ? answer.split(',') : [answer];
+        var correct = true;
+        for (var i = 0; i < values.length; i++) {
+            if (answers.indexOf(values[i]) == -1) {
+                correct = false;
+                break;
             }
         }
-      }
-    }
-
-    if(data){
-      createQuizHtml(this.container, data);
-    }
-    this.container.on('change', 'input, select', function(){handler($(this))})
-    .find('.right, .wrong').hide();
-}
+        var resultSpans = container.querySelectorAll('.right, .wrong');
+        for (var i = 0; i < resultSpans.length; i++) {
+            resultSpans[i].style.display = 'none';
+        }
+        if ((correct && (values.length == answers.length))) {
+            container.querySelector('.right').style.display = '';
+        }
+        else {
+            container.querySelector('.wrong').style.display = '';
+        }
+    };
+    ;
+    Quizly.prototype.createQuizHtml = function (container, data) {
+        for (var i = 0; i < data.length; i++) {
+            var question = data[i];
+            if (question.type == "select") {
+                this.template.createSelect(question);
+            }
+            else {
+                if (question.type == "radio" || question.type == "checkbox") {
+                    this.template.createCheckboxOrRadio(question);
+                }
+                else {
+                    this.template.createInput(question);
+                }
+            }
+        }
+    };
+    return Quizly;
+}());
--- src/template.js
+++ src/template.js
@@ -1,73 +1,92 @@
-function QuizlyTemplate(container){
-  this.container = container;
-
-  createLabel = function(question, includeRightWrong, right, wrong){
-    return $(document.createElement('label'))
-    .append(question)
-    .append(includeRightWrong ? createRightWrong(right, wrong) : '')
-    .append('<br>');
-  }
-  createSelectElement = function(answer, name){
-    return $(document.createElement('select'))
-      .attr('data-answer', answer)
-      .attr('name', name);
-  }
-  createOption = function(value, text){
-    return $(document.createElement('option'))
-      .val(value)
-      .text(text);
-  }
-  createQuestionContainer = function(text, answer, right, wrong){
-    return $(document.createElement('div')).attr('data-quiz-container', '').append(
-      $(document.createElement('span'))
-        .text(text)
-        .attr('data-answer', answer)
-    )
-    .append(createRightWrong(right, wrong))
-    .append('<br>');
-  }
-  createInput = function(type, name){
-    return $(document.createElement('input'))
-      .attr('type', type)
-      .attr('name', name);
-  }
-  createRightWrong = function(right, wrong){
-    return [
-      $(document.createElement('span'))
-      .text(right ? right : "Correct").addClass('right'),
-      $(document.createElement('span'))
-        .text(wrong ? wrong : "Incorrect").addClass('wrong')
-      ];
-  }
-}
-
-QuizlyTemplate.prototype.createSelect = function(question){
-    var answer = question.answers.length ? question.answers.join(',') : question.answer;
-    var $select = createSelectElement(answer, question.name);
-    for(var j = 0; j < question.values.length; j++){
-      var $option = createOption(question.values[j], question.values[j]);
-      $select.append($option);
+var QuizlyTemplater = (function () {
+    function QuizlyTemplater(container) {
+        this.container = container;
     }
-    this.container.append(createLabel(question.question, true, question.right, question.wrong)
-    .append($select)
-    .append('<br>'));
-}
-
-QuizlyTemplate.prototype.createCheckboxOrRadio = function(question){
-    var $questionContainer = createQuestionContainer(question.question, question.answers.length ? question.answers.join(',') : question.answer, question.right, question.wrong);
-    this.container.append($questionContainer);
-    for(var j = 0; j < question.values.length; j++){
-      var value = question.values[j];
-      var $label = createLabel(typeof value === 'object' ? value.label : value)
-        .prepend(createInput(question.type, question.name).val(typeof value === 'object' ? value.value : value));
-      $questionContainer.append($label);
-    }
-}
-
-QuizlyTemplate.prototype.createInput = function(question){
-    this.container.append(createLabel(question.question, true, question.right, question.wrong).append(
-      createInput(question.type, question.name)
-        .attr('data-answer', question.answer)
-        .attr('placeholder', question.placeholder)
-    ));
-}
+    QuizlyTemplater.prototype.createLabel = function (question, includeRightWrong, right, wrong) {
+        var label = document.createElement('label');
+        label.textContent = question;
+        if (includeRightWrong) {
+            var rightAndWrong = this.createRightWrong(right, wrong);
+            for (var i = 0; i < rightAndWrong.length; i++) {
+                label.appendChild(rightAndWrong[i]);
+            }
+        }
+        var br = document.createElement('br');
+        label.appendChild(br);
+        return label;
+    };
+    QuizlyTemplater.prototype.createSelectElement = function (answer, name) {
+        var select = document.createElement('select');
+        select.setAttribute('data-answer', answer);
+        select.setAttribute('name', name);
+        return select;
+    };
+    QuizlyTemplater.prototype.createOption = function (value, text) {
+        var option = document.createElement('option');
+        option.value = value;
+        option.textContent = text;
+        return option;
+    };
+    QuizlyTemplater.prototype.createQuestionContainer = function (text, answer, right, wrong) {
+        var div = document.createElement('div');
+        div.setAttribute('data-quiz-container', '');
+        var span = document.createElement('span');
+        span.textContent = text;
+        span.setAttribute('data-answer', answer);
+        var rightAndWrong = this.createRightWrong(right, wrong);
+        for (var i = 0; i < rightAndWrong.length; i++) {
+            span.appendChild(rightAndWrong[i]);
+        }
+        div.appendChild(span);
+        div.appendChild(document.createElement('br'));
+        return div;
+    };
+    QuizlyTemplater.prototype.createInputElement = function (type, name) {
+        var input = document.createElement('input');
+        input.setAttribute('type', type);
+        input.setAttribute('name', name);
+        return input;
+    };
+    QuizlyTemplater.prototype.createRightWrong = function (rightText, wrongText) {
+        var right = document.createElement('span');
+        right.textContent = rightText ? rightText : "Correct";
+        right.classList.add('right');
+        var wrong = document.createElement('span');
+        wrong.textContent = wrongText ? wrongText : "Incorrect";
+        wrong.classList.add('wrong');
+        return [right, wrong];
+    };
+    QuizlyTemplater.prototype.createSelect = function (question) {
+        var answer = question.answers.length ? question.answers.join(',') : question.answer;
+        var select = this.createSelectElement(answer, question.name);
+        for (var j = 0; j < question.values.length; j++) {
+            var option = this.createOption(question.values[j], question.values[j]);
+            select.appendChild(option);
+        }
+        var label = this.createLabel(question.question, true, question.right, question.wrong);
+        label.appendChild(select);
+        label.appendChild(document.createElement('br'));
+        this.container.appendChild(label);
+    };
+    QuizlyTemplater.prototype.createCheckboxOrRadio = function (question) {
+        var questionContainer = this.createQuestionContainer(question.question, question.answers.length ? question.answers.join(',') : question.answer, question.right, question.wrong);
+        this.container.appendChild(questionContainer);
+        for (var j = 0; j < question.values.length; j++) {
+            var value = question.values[j];
+            var label = this.createLabel(typeof value === 'object' ? value.label : value);
+            var input = this.createInputElement(question.type, question.name);
+            input.value = typeof value === 'object' ? value.value : value;
+            label.insertBefore(input, label.firstChild);
+            questionContainer.appendChild(label);
+        }
+    };
+    QuizlyTemplater.prototype.createInput = function (question) {
+        var label = this.createLabel(question.question, true, question.right, question.wrong);
+        var input = this.createInputElement(question.type, question.name);
+        input.setAttribute('data-answer', question.answer);
+        input.setAttribute('placeholder', question.placeholder);
+        label.appendChild(input);
+        this.container.appendChild(label);
+    };
+    return QuizlyTemplater;
+}());
